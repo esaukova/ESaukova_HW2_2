87f2ad991b6f6e031d029fad5a30fea10a86c680
Ekaterina Saukova <esaukova@outlook.com>
2023-06-16 21:16:08 +0300

    Расчет бонусов при пополнение счета

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    /* Объявляете переменные для входных данных и
+       параметров программы: начального счёта,
+       суммы пополнения и тп
+
+       Условным оператором проверяете, превысила ли
+       сумма пополнения порог, и для этих двух разных
+       сценариев рассчитываете сумму бонуса и выводите
+       на экран. */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите начальную сумму на счету: ");
+        int initAmount = scanner.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите сумму пополнения: ");
+        int refillAmount = sc.nextInt();
+
+        int bonus = refillAmount / 100;
+        int finalAmount = initAmount + refillAmount + bonus;
+
+        if (bonus >= 1) {
+            System.out.println("Клиент пополнил счёт на: " + refillAmount + " рублей - бонус равен: " + bonus + " рублей, итоговая сумма на счету клиента: " + finalAmount + " рублей.");
+        } else {
+            System.out.println("Клиент пополнил счёт на: " + refillAmount + " рублей - бонусов нет, итоговая сумма на счету клиента: " + finalAmount + " рублей.");
+        }
+
+    }
+
+}
